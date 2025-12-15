fa4d2be66895b9b9f62b0f53e208b08f4f1f8792
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-12-15 16:28:25 -0500

    Remove unused

--- R/select_assets.R
+++
@@ -1,7 +0,0 @@
-select_asset <- function(item, asset_id) {
-  # Check for "item" class
-  # TODO
-
-  # Extract entry
-  item[["assets"]][[asset_id]]
-}
--- R/select_collection.R
+++
@@ -1,5 +0,0 @@
-select_collection <- function(collection_id) {
-  rstac::stac(stac_url) |>
-    rstac::collections(collection_id = collection_id) |>
-    rstac::get_request()
-}
