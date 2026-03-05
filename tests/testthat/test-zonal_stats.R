bcff13f68128aa57516b5a6a9b7a17b72afe6938
Sharla Gelfand <sharla.gelfand@gmail.com>
2026-03-04 20:58:26 -0500

    Test scenarios

--- tests/testthat/test-zonal_stats.R
+++ tests/testthat/test-zonal_stats.R
@@ -13,16 +13,90 @@
 # })
 
 test_that("get_zonal_statistics allows using covariate name or ID", {
-  se <- mermaidr::mermaid_get_project_data("4d23d2a1-774f-4ccf-b567-69f95e4ff572", "fishbelt", "sampleevents", limit = 1)
-
-  zs_name <- get_zonal_statistics(se, "Daily Sea Surface Temperature",
-    n_days = 1,
-    buffer = 10, stats = "mean"
+  se <- mermaidr::mermaid_get_project_data(
+    "4d23d2a1-774f-4ccf-b567-69f95e4ff572",
+    "fishbelt",
+    "sampleevents",
+    limit = 1
   )
-  zs_id <- get_zonal_statistics(se, "50b810fb-5f17-4cdb-b34b-c377837e2a29",
+
+  zs_name <- get_zonal_statistics(
+    se,
+    "Daily Sea Surface Temperature",
     n_days = 1,
-    buffer = 10, stats = "mean"
+    radius = 10,
+    spatial_stats = "mean"
+  )
+
+  zs_id <- get_zonal_statistics(
+    se,
+    "50b810fb-5f17-4cdb-b34b-c377837e2a29",
+    n_days = 1,
+    radius = 10,
+    spatial_stats = "mean"
   )
 
   expect_identical(zs_name, zs_id)
+})
+
+test_that("NA returned for value ONLY when there is data on date, but not within radius", {
+  # No data within 1000m of this site, known
+  se <- mermaidr::mermaid_get_project_data(
+    "75ef7a5a-c770-4ca6-b9f8-830cab74e425",
+    "benthicpit",
+    "sampleevents",
+    token = NULL
+  ) %>%
+    dplyr::filter(
+      site == "MAP28",
+      sample_date == "2010-05-07"
+    )
+
+  covariates <- get_zonal_statistics(se,
+    "Daily Sea Surface Temperature",
+    n_days = 1,
+    radius = 1000,
+    spatial_stats = "mean"
+  )
+
+  expect_true(covariates %>%
+    dplyr::pull(covariates) %>%
+    purrr::map_dbl("value") %>%
+    is.na())
+
+  expect_false(covariates %>%
+    dplyr::pull(covariates) %>%
+    purrr::map_chr("date") %>%
+    is.na())
+})
+
+test_that("NA returned for all columns when there is no data within date range", {
+  se <- mermaidr::mermaid_get_project_data(
+    "4d23d2a1-774f-4ccf-b567-69f95e4ff572",
+    "fishbelt",
+    "sampleevents",
+    limit = 1
+  )
+
+  se <- se %>%
+    dplyr::mutate(sample_date = as.Date("1980-01-01"))
+
+  covariates <- get_zonal_statistics(se,
+    "Daily Sea Surface Temperature",
+    n_days = 1,
+    radius = 10,
+    spatial_stats = "mean"
+  )
+
+  covariates <- covariates %>%
+    dplyr::pull(covariates) %>%
+    purrr::pluck(1)
+
+  expect_true(is.na(covariates[["start_date"]]))
+  expect_true(is.na(covariates[["end_date"]]))
+  expect_true(covariates[["n_dates"]] == 0)
+  expect_true(is.na(covariates[["date"]]))
+  expect_true(covariates[["spatial_stat"]] == "mean")
+  expect_true(is.na(covariates[["start_date"]]))
+  expect_true(is.na(covariates[["value"]]))
 })
